aa60cf9a2b5d80ad929e774dfe4ff0ba1f33ae3c
splanes <sergio.planes@adevinta.com>
2025-03-05 16:33:14 +0100

    Initial commit

---
+++ PetChat/presentation/src/main/java/com/splanes/apps/petchat/presentation/common/theme/PetChatTheme.kt
@@ -0,0 +1,11 @@
+package com.splanes.apps.petchat.presentation.common.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PetChatTheme(
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable () -> Unit
+) {
+
+}
