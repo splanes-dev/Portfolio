d3c6e987b9efc01ab3c8c1cf15e6548486d5123f
splanes <splanes@protonmail.com>
2025-03-05 17:15:09 +0100

    PetChat: colorScheme

--- PetChat/presentation/build.gradle.kts
+++ PetChat/presentation/build.gradle.kts
@@ -45,8 +45,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
 
-
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
