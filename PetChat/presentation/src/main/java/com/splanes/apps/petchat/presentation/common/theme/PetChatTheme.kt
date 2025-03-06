da0ec15d6d44fd90abba5fae15ea1f3c85d4bc6b
splanes <splanes@protonmail.com>
2025-03-06 13:52:04 +0100

    PetChat: theme config

--- PetChat/presentation/src/main/java/com/splanes/apps/petchat/presentation/common/theme/PetChatTheme.kt
+++ PetChat/presentation/src/main/java/com/splanes/apps/petchat/presentation/common/theme/PetChatTheme.kt
@@ -1,11 +1,62 @@
 package com.splanes.apps.petchat.presentation.common.theme
 
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import com.splanes.apps.petchat.presentation.common.theme.colors.AppColorScheme
+import com.splanes.apps.petchat.presentation.common.theme.colors.DarkColorScheme
+import com.splanes.apps.petchat.presentation.common.theme.colors.LightColorScheme
+import com.splanes.apps.petchat.presentation.common.theme.typography.Typography
+
+internal val LocalAppColorScheme = staticCompositionLocalOf<AppColorScheme> {
+  error("No AppColorScheme provided")
+}
+
+object PetChatTheme {
+  val colors: AppColorScheme
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalAppColorScheme.current
+
+  val typography: Typography
+    @Composable
+    @ReadOnlyComposable
+    get() = MaterialTheme.typography
+}
 
 @Composable
 fun PetChatTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable () -> Unit
 ) {
+  val colorScheme = when {
+    darkTheme -> DarkColorScheme
+    else -> LightColorScheme
+  }
 
+  val view = LocalView.current
+  if (!view.isInEditMode) {
+    SideEffect {
+      val window = (view.context as Activity).window
+      window.statusBarColor = colorScheme.background.toArgb()
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+    }
+  }
+
+  CompositionLocalProvider(LocalAppColorScheme provides colorScheme) {
+    MaterialTheme(
+      colorScheme = colorScheme.toMaterialColorScheme(),
+      typography = Typography,
+      content = content
+    )
+  }
 }
---
+++ PetChat/presentation/src/main/java/com/splanes/apps/petchat/presentation/common/theme/typography/Typography.kt
@@ -0,0 +1,45 @@
+package com.splanes.apps.petchat.presentation.common.theme.typography
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.splanes.apps.petchat.presentation.R
+
+private val baseline = Typography()
+private val displayFontFamily: FontFamily = FontFamily(
+  Font(resId = R.font.apercu_pro_regular),
+  Font(resId = R.font.apercu_pro_bold, weight = FontWeight.Bold),
+  Font(resId = R.font.apercu_pro_light, weight = FontWeight.Light)
+)
+private val bodyFontFamily: FontFamily = FontFamily(
+  Font(resId = R.font.product_sans_regular),
+  Font(resId = R.font.product_sans_bold, weight = FontWeight.Bold),
+  Font(resId = R.font.product_sans_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+  Font(resId = R.font.product_sans_italic, style = FontStyle.Italic),
+  Font(resId = R.font.product_sans_light, weight = FontWeight.Light),
+  Font(
+    resId = R.font.product_sans_light_italic,
+    weight = FontWeight.Light,
+    style = FontStyle.Italic
+  ),
+)
+
+internal val Typography = Typography(
+  displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+  displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+  displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+  headlineLarge = baseline.headlineLarge.copy(fontFamily = bodyFontFamily),
+  headlineMedium = baseline.headlineMedium.copy(fontFamily = bodyFontFamily),
+  headlineSmall = baseline.headlineSmall.copy(fontFamily = bodyFontFamily),
+  titleLarge = baseline.titleLarge.copy(fontFamily = bodyFontFamily),
+  titleMedium = baseline.titleMedium.copy(fontFamily = bodyFontFamily),
+  titleSmall = baseline.titleSmall.copy(fontFamily = bodyFontFamily),
+  bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+  bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+  bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+  labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
+  labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
+  labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+)
